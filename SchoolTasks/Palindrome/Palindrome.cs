16f736c9baa153ab396b17b28988bd89f2f03b4a
Alexander Bayanov <ialex.real@gmail.com>
2019-07-26 23:56:55 +0700

    Made palindrome

--- SchoolTasks/Palindrome/Palindrome.cs
+++ SchoolTasks/Palindrome/Palindrome.cs
@@ -6,6 +6,37 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
+            Console.Write("введите строку: ");
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine("это " + (IsPalindrome(input) ? "" : "не") + " палиндром");
+        }
+
+        static bool IsPalindrome(string input)
+        {
+            for (int leftIndex = 0, rightIndex = input.Length - 1; leftIndex <= rightIndex; leftIndex++, rightIndex--)
+            {
+                while (leftIndex < input.Length && char.IsWhiteSpace(input[leftIndex]))
+                {
+                    leftIndex++;
+                }
+
+                while (rightIndex >= 0 && char.IsWhiteSpace(input[rightIndex]))
+                {
+                    rightIndex--;
+                }
+
+                if (leftIndex < input.Length && rightIndex >= 0)
+                {
+                    if (char.ToLower(input[leftIndex]) != char.ToLower(input[rightIndex]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
