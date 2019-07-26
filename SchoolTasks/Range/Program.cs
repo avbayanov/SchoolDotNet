d328e7c0a4b81ff05488cc322f408b78dfb27c17
Alexander Bayanov <ialex.real@gmail.com>
2019-07-26 23:12:46 +0700

    Made Range

---
+++ SchoolTasks/Range/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Range
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Range range = new Range(10, 20);
+
+            Console.WriteLine("length of range [10, 20]: " + range.GetLength());
+
+            CheckForInsideRangeAndPrint(range, 20);
+        }
+
+        static void CheckForInsideRangeAndPrint(Range range, double input)
+        {
+            Console.WriteLine(input + " is " + (range.IsInside(input) ? "inside" : "outside") + " of range");
+        }
+    }
+}
---
+++ SchoolTasks/Range/Range.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Range
+{
+    class Range
+    {
+        private double from;
+        private double to;
+
+        public Range(int from, int to)
+        {
+            if (from < to)
+            {
+                this.from = from;
+                this.to = to;
+            }
+            else
+            {
+                this.from = to;
+                this.to = from;
+            }
+        }
+
+        public double GetLength()
+        {
+            return to - from;
+        }
+
+        public bool IsInside(double input)
+        {
+            double epsilon = 1.0e-10;
+
+            if ((from - epsilon <= input) && (to + epsilon >= input))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
