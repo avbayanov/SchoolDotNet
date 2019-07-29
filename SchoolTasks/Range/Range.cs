12ac76574ae53241a60475fae3178992f2b0c5ff
Alexander <Alexander@DESKTOP-8JK6KK0>
2019-07-29 18:20:15 +0700

    Simplified IsInside

--- SchoolTasks/Range/Range.cs
+++ SchoolTasks/Range/Range.cs
@@ -28,14 +28,7 @@ namespace Range
 
         public bool IsInside(double input)
         {
-            double epsilon = 1.0e-10;
-
-            if ((from - epsilon <= input) && (to + epsilon >= input))
-            {
-                return true;
-            }
-
-            return false;
+            return input >= from && input <= to;
         }
     }
 }
