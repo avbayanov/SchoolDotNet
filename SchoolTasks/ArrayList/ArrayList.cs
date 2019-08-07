156c7e2849f9ff0a573fdcfe73a1f69a93627f50
Alexander <Alexander@DESKTOP-8JK6KK0>
2019-08-07 18:40:33 +0700

    Made final version

--- SchoolTasks/ArrayList/ArrayList.cs
+++ SchoolTasks/ArrayList/ArrayList.cs
@@ -1,70 +1,202 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ArrayList
 {
     class ArrayList<T> : IList<T>
     {
-        public T this[int index]
+        private T[] array;
+        private int modCount;
+
+        public int Count { get; private set; }
+        public bool IsReadOnly => false;
+
+        public ArrayList()
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            array = new T[10];
         }
 
-        public int Count { get; }
-        public bool IsReadOnly { get; }
+        public ArrayList(int capacity)
+        {
+            array = new T[capacity];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), "Index must be >= 0 and < ArrayList.Count");
+                }
+
+                return array[index];
+            }
+
+            set
+            {
+                if (index < 0 || index >= Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), "Index must be >= 0 and < ArrayList.Count");
+                }
+
+                array[index] = value;
+
+                modCount++;
+            }
+        }
+
+        private void ResizeArray(int arrayLength)
+        {
+            T[] temp = new T[arrayLength];
+
+            Array.Copy(array, 0, temp, 0, Math.Min(Count, arrayLength));
+
+            array = temp;
+        }
+
+        private void CheckForFullAndDoubleCapacity()
+        {
+            if (Count == array.Length)
+            {
+                ResizeArray(array.Length * 2 + 1);
+            }
+        }
+
+        public void EnsureCapacity(int capacity)
+        {
+            if (capacity > array.Length)
+            {
+                ResizeArray(capacity);
+            }
+        }
+
+        public void TrimToCount()
+        {
+            ResizeArray(Count);
+        }
+
         public void Clear()
         {
-            throw new NotImplementedException();
+            Count = 0;
+
+            modCount++;
         }
 
         public int IndexOf(T item)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < Count; i++)
+            {
+                if (array[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            return IndexOf(item) != -1;
         }
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            CheckForFullAndDoubleCapacity();
+
+            array[Count] = item;
+
+            Count++;
+
+            modCount++;
         }
 
         public void Insert(int index, T item)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index >= Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "Index must be >= 0 and < ArrayList.Count");
+            }
+
+            CheckForFullAndDoubleCapacity();
+
+            Array.Copy(array, index, array, index + 1, Count - index);
+
+            array[index] = item;
+
+            Count++;
+
+            modCount++;
         }
 
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            int index = IndexOf(item);
+
+            if (index == -1)
+            {
+                return false;
+            }
+
+            RemoveAt(IndexOf(item));
+
+            return true;
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index >= Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "Index must be >= 0 and < ArrayList.Count");
+            }
+
+            Array.Copy(array, index + 1, array, index, Count - index - 1);
+
+            Count--;
+
+            modCount++;
         }
 
         public void CopyTo(T[] array, int arrayIndex)
         {
-            throw new NotImplementedException();
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array), "array must be not null");
+            }
+
+            if (arrayIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex), "arrayIndex must be >= 0");
+            }
+
+            if (Count + arrayIndex > array.Length)
+            {
+                throw new ArgumentException("Not enough space in array: (Count + arrayIndex) must be <= array.Length");
+            }
+
+            Array.Copy(this.array, 0, array, arrayIndex, Count);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            int enumeratorModCount = modCount;
+
+            for (int i = 0; i < Count; i++)
+            {
+                if (enumeratorModCount != modCount)
+                {
+                    throw new InvalidOperationException("Collection was modified");
+                }
+
+                yield return array[i];
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
-        }
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            throw new NotImplementedException();
         }
     }
 }
--- SchoolTasks/ArrayList/Program.cs
+++ SchoolTasks/ArrayList/Program.cs
@@ -7,8 +7,38 @@ namespace ArrayList
     {
         static void Main(string[] args)
         {
-            IList<int> newInts = new ArrayList<int> {1, 2, 3};
-            Console.WriteLine(newInts);
+            IList<int> integers = new ArrayList<int>();
+            integers.Add(1);
+            integers.Add(2);
+            integers.Add(3);
+
+            Console.WriteLine("integers: " + String.Join(", ", integers));
+
+            Console.WriteLine("integers.Count = " + integers.Count);
+            Console.WriteLine("integers[1] = " + integers[1]);
+            Console.WriteLine("integers.IndexOf(3) = " + integers.IndexOf(3));
+
+            Console.WriteLine("inserting '5' to index 1");
+            integers.Insert(1, 5);
+            Console.WriteLine("integers: " + String.Join(", ", integers));
+
+            Console.WriteLine("removing '1'");
+            integers.Remove(1);
+            Console.WriteLine("integers: " + String.Join(", ", integers));
+
+            Console.WriteLine("copying integers to array and shifting elements 1 position right");
+            int[] array = new int[integers.Count + 1];
+            integers.CopyTo(array, 1);
+            Console.WriteLine("array: " + String.Join(", ", array));
+
+            ArrayList<int> integers1 = new ArrayList<int>(0);
+            integers1.EnsureCapacity(1);
+            integers1.Add(1);
+            integers1.Add(2);
+            integers1.Add(3);
+            integers1.TrimToCount();
+
+            Console.WriteLine("integers1: " + String.Join(", ", integers1));
         }
     }
 }
