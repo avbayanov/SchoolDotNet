800401159b54c87cc8de2d2f37d92ed3a4219b6e
Alexander <Alexander@DESKTOP-8JK6KK0>
2019-08-08 21:14:07 +0700

    Replaced String with string

--- SchoolTasks/HashTable/Program.cs
+++ SchoolTasks/HashTable/Program.cs
@@ -13,22 +13,22 @@ namespace HashTable
             integers.Add(78);
             integers.Add(99);
 
-            Console.WriteLine("integers: " + String.Join(", ", integers));
+            Console.WriteLine("integers: " + string.Join(", ", integers));
 
             Console.WriteLine("integers.Contains(78) = " + integers.Contains(78));
 
             Console.WriteLine("removing '55'");
             integers.Remove(55);
-            Console.WriteLine("integers: " + String.Join(", ", integers));
+            Console.WriteLine("integers: " + string.Join(", ", integers));
 
             Console.WriteLine("copying integers to array and shifting elements 1 position right");
             var array = new int[integers.Count + 1];
             integers.CopyTo(array, 1);
-            Console.WriteLine("array: " + String.Join(", ", array));
+            Console.WriteLine("array: " + string.Join(", ", array));
 
             Console.WriteLine("clearing integers");
             integers.Clear();
-            Console.WriteLine("integers: " + String.Join(", ", integers));
+            Console.WriteLine("integers: " + string.Join(", ", integers));
         }
     }
 }
