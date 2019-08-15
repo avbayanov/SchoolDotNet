6f501286edb81abb57066cede2b54f0bf8a6a41a
Alexander <Alexander@DESKTOP-8JK6KK0>
2019-08-15 19:06:58 +0700

    Made ReadConfig

---
+++ SchoolTasks/ReadConfig/ReadConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Configuration;
+
+namespace ReadConfig
+{
+    class ReadConfig
+    {
+        static void Main(string[] args)
+        {
+            string siteUrl = ConfigurationManager.AppSettings["SiteUrl"];
+
+            Console.WriteLine("SiteUrl: " + siteUrl);
+        }
+    }
+}
