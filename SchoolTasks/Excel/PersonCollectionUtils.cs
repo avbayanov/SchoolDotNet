535fcea5f69f0565b0c430cdd458b9cd3f1d17f2
Alexander <Alexander@DESKTOP-8JK6KK0>
2019-08-15 20:16:34 +0700

    Made Excel

---
+++ SchoolTasks/Excel/PersonCollectionUtils.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using OfficeOpenXml;
+
+namespace Excel
+{
+    internal static class PersonCollectionUtils
+    {
+        public static void WriteToSpreadsheet(this IEnumerable<Person> persons, FileInfo outputFile)
+        {
+            using (var excelPackage = new ExcelPackage(outputFile))
+            {
+                var worksheet = excelPackage.Workbook.Worksheets.Add("Sheet1");
+
+                worksheet.Column(1).Width = 20;
+                worksheet.Column(2).Width = 20;
+                worksheet.Column(3).Width = 7;
+                worksheet.Column(3).Style.Numberformat.Format = "#";
+
+                worksheet.Cells[1, 1].Value = "First Name";
+                worksheet.Cells[1, 2].Value = "Last Name";
+                worksheet.Cells[1, 3].Value = "Age";
+
+                var row = 1;
+                foreach (var person in persons)
+                {
+                    row++;
+
+                    worksheet.Cells[row, 1].Value = person.FirstName;
+                    worksheet.Cells[row, 2].Value = person.LastName;
+                    worksheet.Cells[row, 3].Value = person.Age;
+                }
+
+                excelPackage.Save();
+            }
+        }
+    }
+}
---
+++ SchoolTasks/Excel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace Excel
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var persons = new Person[]
+            {
+                new Person("FirstName1", "LastName1", 1),
+                new Person("FirstName2", "LastName2", 2),
+                new Person("FirstName3", "LastName3", 3),
+                new Person("FirstName4", "LastName4", 4),
+                new Person("FirstName5", "LastName5", 5),
+                new Person("FirstName6", "LastName6", 6)
+            };
+
+            var outputFile = new FileInfo("output.xlsx");
+
+            persons.WriteToSpreadsheet(outputFile);
+        }
+    }
+}
