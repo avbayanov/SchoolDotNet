9776a2c9d15d4929a76d5d17eb214c0626e575fb
Alexander <Alexander@DESKTOP-8JK6KK0>
2019-09-17 20:33:57 +0700

    Made interfaces and classes for unit of work pattern

---
+++ ShopEf/ShopEf.DataAccess/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ShopEf.DataAccess
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void Save();
+        T GetRepository<T>() where T : class, IRepository;
+    }
+}
---
+++ ShopEf/ShopEf.DataAccess/UnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace ShopEf.DataAccess
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private DbContext _db;
+
+        public UnitOfWork(DbContext db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public T GetRepository<T>() where T : class, IRepository
+        {
+            if (typeof(T) == typeof(IProductRepository))
+            {
+                return new ProductRepository(_db) as T;
+            }
+
+            if (typeof(T) == typeof(ICategoryRepository))
+            {
+                return new CategoryRepository(_db) as T;
+            }
+
+            if (typeof(T) == typeof(ICustomerRepository))
+            {
+                return new CustomerRepository(_db) as T;
+            }
+
+            if (typeof(T) == typeof(IOrderRepository))
+            {
+                return new OrderRepository(_db) as T;
+            }
+
+            if (typeof(T) == typeof(IOrderProductRepository))
+            {
+                return new OrderProductRepository(_db) as T;
+            }
+
+            throw new Exception("Unknown repository type: " + typeof(T));
+        }
+    }
+}
