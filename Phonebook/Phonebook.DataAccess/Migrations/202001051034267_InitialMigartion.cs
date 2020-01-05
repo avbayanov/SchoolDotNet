b375ed0b6e8e973fe7a1c106b16aaaff6e161b3c
Alexander Bayanov <ialex.real@gmail.com>
2020-01-06 02:05:42 +0700

    Added migrations

---
+++ Phonebook/Phonebook.DataAccess/Migrations/202001051034267_InitialMigartion.cs
@@ -0,0 +1,28 @@
+﻿namespace Phonebook.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigartion : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Contacts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(maxLength: 100),
+                        LastName = c.String(maxLength: 100),
+                        PhoneNumber = c.String(maxLength: 30),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Contacts");
+        }
+    }
+}
---
+++ Phonebook/Phonebook.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Phonebook.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    public sealed class Configuration : DbMigrationsConfiguration<Phonebook.DataAccess.PhonebookDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Phonebook.DataAccess.PhonebookDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
