6cbfc1716906c0b4d07187101af2e94ba2e59912
Alexander Bayanov <ialex.real@gmail.com>
2020-01-06 20:03:17 +0700

    Made "Sign assembly"

---
+++ TrySigning/SignedAssembly/Utilities.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SignedAssembly
+{
+    public static class Utilities
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
---
+++ TrySigning/TrySigning/Program.cs
@@ -0,0 +1,12 @@
+﻿using SignedAssembly;
+
+namespace TrySigning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utilities.SayHello();
+        }
+    }
+}
