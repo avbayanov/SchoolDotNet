eec130899dd5941be83283f3be565d6a2fa6e8d7
Alexander Bayanov <ialex.real@gmail.com>
2020-01-06 22:24:54 +0700

    Made "Log"

---
+++ SchoolTasks/Logging/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using NLog;
+
+namespace Logging
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+
+            logger.Info("Program launched");
+
+            logger.Debug("Trying to read secrets");
+            try
+            {
+                using (var reader = new StreamReader("secrets.txt"))
+                {
+                    var secrets = reader.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                logger.Error(e);
+            }
+        }
+    }
+}
