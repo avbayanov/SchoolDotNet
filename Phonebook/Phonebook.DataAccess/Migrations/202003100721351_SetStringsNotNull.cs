ca06fb4efb8fd880cca3d08f97c6434a1f8621a6
Alexander <Alexander@DESKTOP-8JK6KK0>
2020-03-10 14:23:33 +0700

    Add migartion for nut null strings

---
+++ Phonebook/Phonebook.DataAccess/Migrations/202003100721351_SetStringsNotNull.cs
@@ -0,0 +1,22 @@
+﻿namespace Phonebook.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SetStringsNotNull : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Contacts", "FirstName", c => c.String(nullable: false, maxLength: 100));
+            AlterColumn("dbo.Contacts", "LastName", c => c.String(nullable: false, maxLength: 100));
+            AlterColumn("dbo.Contacts", "PhoneNumber", c => c.String(nullable: false, maxLength: 30));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Contacts", "PhoneNumber", c => c.String(maxLength: 30));
+            AlterColumn("dbo.Contacts", "LastName", c => c.String(maxLength: 100));
+            AlterColumn("dbo.Contacts", "FirstName", c => c.String(maxLength: 100));
+        }
+    }
+}
